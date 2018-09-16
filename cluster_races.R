39a7da52071eb8bed4a53d4a05eae7e4057feb7e
danic <danic@sfu.ca>
2018-09-15 23:46:53 -0700

    filtered out more bad races. Set seed for repoducibility. Wrote paper up to results section

--- cluster_races.R
+++ cluster_races.R
@@ -3,7 +3,6 @@
 #####################
 # If pacman is not installed, install it
 # install.packages('pacman')
-# for longclust https://ms.mcmaster.ca/~paul/software.html
 pacman::p_load(tidyverse, glue, lubridate, dtwclust)
 
 races_full <- read_csv("rowing_world_championships.csv")
@@ -14,6 +13,7 @@ races_cleaned <-
   # there are gps errors where the average speed recorded is less than the true speed
   # this may be changed later but is a good baseline
   filter_at(vars(matches("speed")), all_vars(!is.na(.) & . > 2)) %>%
+  filter(dnf == FALSE, exc == FALSE) %>%
   mutate(row_number = row_number())
 
 races_long <- 
@@ -30,37 +30,17 @@ races_standardized <-
   select(-speed) %>%
   mutate(distance = paste0("speed_", distance)) %>%
   spread(distance, speed_standard) %>%
-  select(row_number, matches("speed_\\d{2}$"), matches("speed_\\d{3}$"), matches("speed_\\d{4}$")) %>%
-  mutate_at(vars(-row_number), scale)
-
-# races_smoothed <-
-#   races_long %>%
-#   nest(-row_number) %>%
-#   mutate(loess_model    = map(data, ~ loess(speed ~ distance, data = .x, span = 0.1)),
-#          speed_smoothed = map(loess_model, predict)) %>%
-#   select(-loess_model) %>%
-#   unnest() %>%
-#   select(-speed) %>%
-#   mutate(distance = paste0("speed_", distance)) %>%
-#   spread(distance, speed_smoothed) %>%
-#   select(row_number, matches("speed_\\d{2}$"), matches("speed_\\d{3}$"), matches("speed_\\d{4}$"))
+  select(row_number, matches("speed_\\d{2}$"), matches("speed_\\d{3}$"), matches("speed_\\d{4}$"))
 
 cluster_matrix <-  
   races_standardized %>%
   select(-row_number) %>%
   as.matrix()
 
+set.seed(14)
 # k shape clustering
 # http://www1.cs.columbia.edu/~jopa/Papers/PaparrizosSIGMOD2015.pdf
 clusters <- tsclust(cluster_matrix, k = 4, centroid = "shape", distance = "sbd")
-
-tibble(clust_1 = clusters@centroids[[1]], clust_2 = clusters@centroids[[2]], 
-       clust_3 = clusters@centroids[[3]], clust_4 = clusters@centroids[[4]]) %>%
-  mutate(split_num = row_number()) %>%
-  gather(cluster, std_speed, -split_num) %>%
-  ggplot(aes(x = split_num, y = std_speed, group = cluster)) +
-    geom_line()
-
 
 
 races_cleaned %>%
@@ -68,41 +48,20 @@ races_cleaned %>%
   count(cluster, size) %>% 
   spread(size,  n)
 
-races_standardized %>%
+
+races_plotable <- 
+  races_cleaned %>%
   mutate(cluster = clusters@cluster) %>%
-  select(row_number,  matches("speed_"), cluster) %>%
   gather("distance", "speed", matches("speed")) %>%
   mutate(distance = parse_number(distance),
-         cluster = factor(cluster)) %>%
-  filter(row_number <= 1000) %>%
-  ggplot(aes(x = distance, y = speed, colour = cluster)) +
-  #stat_summary(geom = "point", fun.y = "mean") +
-  #geom_smooth(aes(group = row_number), color = "black", alpha = 0.1, span = 0.25) +
-  #geom_line(aes(group = row_number), color = "black", alpha = 0.1) +
-  stat_summary(geom = "line", fun.y = "mean", size = 1) +
-  #stat_summary(fun.data = "mean_cl_boot", geom = "errorbar") +
-  xlab("Distance (m)") +
-  ylab("Speed (m/s)") +
-  scale_color_discrete("") +
-  theme(legend.position = "bottom") #+
-  facet_wrap(~ cluster)
+         cluster = factor(cluster)) 
 
-
-races_cleaned %>%
-  mutate(cluster = clusters@cluster) %>%
-  filter(junior == "senior", adaptive == FALSE) %>%
-  select(row_number,  matches("speed_"), cluster) %>%
-  gather("distance", "speed", matches("speed")) %>%
-  mutate(distance = parse_number(distance),
-         cluster = factor(cluster)) %>%
-  filter(row_number <= 1000) %>%
+races_plotable %>%
   ggplot(aes(x = distance, y = speed, colour = cluster)) +
-  #stat_summary(geom = "point", fun.y = "mean") +
-  #geom_smooth(aes(group = row_number), color = "black", alpha = 0.1, span = 0.25) +
   stat_summary(geom = "line", fun.y = "mean", size = 1) +
-  #stat_summary(fun.data = "mean_cl_boot", geom = "errorbar") +
   xlab("Distance (m)") +
   ylab("Speed (m/s)") +
   scale_color_discrete("") +
   theme(legend.position = "bottom")
+
 

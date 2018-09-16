30eec8058a4bf3aa3f06aedc307fa7b6d8244784
danic <danic@sfu.ca>
2018-09-16 16:38:28 -0700

    Re-rogranized fil structure again. Added more writing up to Comparing race descriptors

--- analysis/cluster_races.R
+++ analysis/cluster_races.R
@@ -40,28 +40,66 @@ cluster_matrix <-
 set.seed(14)
 # k shape clustering
 # http://www1.cs.columbia.edu/~jopa/Papers/PaparrizosSIGMOD2015.pdf
-clusters <- tsclust(cluster_matrix, k = 4, centroid = "shape", distance = "sbd")
+clusters_4 <- tsclust(cluster_matrix, k = 4, centroid = "shape", distance = "sbd")
+clusters_3 <- tsclust(cluster_matrix, k = 3, centroid = "shape", distance = "sbd")
+clusters_5 <- tsclust(cluster_matrix, k = 5, centroid = "shape", distance = "sbd")
 
+cluster_centroids <- 
+  tibble(centroids = clusters_4@centroids, num_clusters = 4) %>%
+  bind_rows(tibble(centroids = clusters_5@centroids, num_clusters = 5)) %>%
+  bind_rows(tibble(centroids = clusters_3@centroids, num_clusters = 3)) %>%
+  group_by(num_clusters) %>%
+  mutate(cluster = row_number()) %>%
+  unite(cluster_num_clusters, cluster, num_clusters) %>%
+  spread(cluster_num_clusters, centroids) %>%
+  unnest() %>%
+  mutate(split = row_number() * 50) %>%
+  gather(cluster_num_clusters, speed, matches("\\d_\\d")) %>%
+  separate(cluster_num_clusters, c("cluster", "num_clusters"), sep = "_")
 
-races_cleaned %>%
-  mutate(cluster = clusters@cluster) %>%
+write_rds(cluster_centroids, "paper/data/cluster_centroids.rds")
+
+cluster_summary <- 
+  clusters_4@clusinfo %>% 
+  as_tibble() %>% 
+  mutate(Cluster = row_number()) %>% 
+  select(Cluster, Size = size, `Average Distance` = av_dist)
+
+write_rds(cluster_summary, "paper/data/cluster_summary.rds")
+
+races_clustered <- 
+  races_cleaned %>%
+  mutate(cluster = clusters_4@cluster,
+         cluster = case_when(cluster == 1 ~ "U-Shaped",
+                             cluster == 2 ~ "Even",
+                             cluster == 3 ~ "Positive",
+                             cluster == 4 ~ "Reverse J-Shaped")) %>%
+  left_join(races_standardized, by = "row_number", suffix = c("", "_std"))
+  
+
+write_rds(races_clustered, "paper/data/races_clustered.rds")
+
+races_clustered %>%
+  count(cluster, adapt_desig) %>% 
+  spread(adapt_desig,  n)
+
+races_clustered %>%
   count(cluster, size) %>% 
   spread(size,  n)
 
 
 races_plotable <- 
-  races_cleaned %>%
-  mutate(cluster = clusters@cluster) %>%
-  gather("distance", "speed", matches("speed")) %>%
-  mutate(distance = parse_number(distance),
-         cluster = factor(cluster)) 
+  races_clustered %>%
+  gather("distance", "speed", matches("speed_\\d{2,4}_std")) %>%
+  mutate(distance = parse_number(distance)) 
+
+write_rds(races_plotable, "paper/data/races_plotable.rds")
 
 races_plotable %>%
-  ggplot(aes(x = distance, y = speed, colour = cluster)) +
-  stat_summary(geom = "line", fun.y = "mean", size = 1) +
+  ggplot(aes(x = distance, y = speed, colour = cluster, group = row_number)) +
+  geom_line(alpha = 0.05) +
   xlab("Distance (m)") +
   ylab("Speed (m/s)") +
-  scale_color_discrete("") +
-  theme(legend.position = "bottom")
-
+  scale_color_manual("Cluster", values = colour_palette) +
+  facet_wrap(~ cluster)
 
